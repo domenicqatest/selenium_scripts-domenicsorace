e769ca4db71431a0f3c3a11aa57a415a004ddf5e
domenicqatest <domenicqatest@gmail.com>
2014-07-21 12:54:05 -0400

    Filtering_Breadcrumb Update

--- Filtering_Breadcrumb_FilterReset.java
+++ Filtering_Breadcrumb_FilterReset.java
@@ -28,38 +28,60 @@ public class Filtering_Breadcrumb_FilterReset {
     driver.findElement(By.cssSelector("button.btn.dropdown-toggle")).click();
     driver.findElement(By.linkText("Display")).click();
     
+  //Pause//
+    Thread.sleep(3000);
+    
     assertEquals("DISPLAY", driver.findElement(By.cssSelector("div.filter-text")).getText());
     driver.findElement(By.xpath("//div[@id='search']/div/div/div/div[3]/div/button[2]")).click();
     driver.findElement(By.linkText("CPG")).click();
+    
+  //Pause//
+    Thread.sleep(3000);
 
     
     assertEquals("CPG", driver.findElement(By.xpath("//div[@id='filter-tags']/div[2]/div")).getText());
     driver.findElement(By.xpath("//div[@id='search']/div/div/div/div[4]/div/button[2]")).click();
     driver.findElement(By.linkText("Video")).click();
 
+  //Pause//
+    Thread.sleep(3000);
     
     assertEquals("VIDEO", driver.findElement(By.xpath("//div[@id='filter-tags']/div[3]/div")).getText());
     driver.findElement(By.xpath("//div[@id='search']/div/div/div/div[5]/div/button[2]")).click();
     driver.findElement(By.linkText("300x250")).click();
 
+  //Pause//
+    Thread.sleep(3000);
     
     assertEquals("300X250", driver.findElement(By.xpath("//div[@id='filter-tags']/div[4]/div")).getText());
     driver.findElement(By.xpath("//div[@id='search']/div/div/div/div[6]/div/button[2]")).click();
+    
+  //Pause to look at Country dropdown//
+    Thread.sleep(3000);
+    
     driver.findElement(By.linkText("United States")).click();
   
+  //Pause//
+    Thread.sleep(3000);
     
     assertEquals("UNITED STATES", driver.findElement(By.xpath("//div[@id='filter-tags']/div[5]/div")).getText());
     // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | null | ]]
-    driver.findElement(By.xpath("(//a[contains(text(),'300 x 250')])[85]")).click();
+    driver.findElement(By.xpath("(//a[contains(text(),'300 x 250')])[82]")).click();
     
+  //Pause//
+    Thread.sleep(3000);
     
     assertEquals("Clear Haircare - 300x250", driver.findElement(By.cssSelector("li.active")).getText());
     driver.findElement(By.linkText("CPG")).click();
 
+  //Pause//
+    Thread.sleep(3000);
     
     assertEquals("CPG", driver.findElement(By.cssSelector("div.filter-text")).getText());
     driver.findElement(By.cssSelector("div.remove-filter")).click();
     
+  //Pause//
+    Thread.sleep(8000);
     
     assertEquals("Spongecell", driver.getTitle());
   }
