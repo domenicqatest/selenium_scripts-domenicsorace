42ed4cd1a9e5a97851a1d16d53ebbae8a9a2441d
domenicqatest <domenicqatest@gmail.com>
2014-07-21 12:10:10 -0400

    Dynamic Update

--- Dynamic.java
+++ Dynamic.java
@@ -1,3 +1,7 @@
+
+//You must actually watch this script run in order to verify the dropdowns are working accurately.//
+
+
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
@@ -27,48 +31,93 @@ public class Dynamic {
     assertTrue(isElementPresent(By.cssSelector("button.btn.dropdown-text")));
     driver.findElement(By.cssSelector("button.btn.dropdown-toggle")).click();
     driver.findElement(By.linkText("Chicago, IL, US")).click();
+    
+    //Pause Test//
+    Thread.sleep(6000);
+
+    
     assertEquals("CHICAGO, IL, US", driver.findElement(By.cssSelector("div.filter-text")).getText());
     assertTrue(isElementPresent(By.cssSelector("div.filter-text")));
     driver.findElement(By.cssSelector("button.btn.dropdown-toggle")).click();
     driver.findElement(By.linkText("Houston, TX, US")).click();
+    
+  //Pause Test//
+    Thread.sleep(6000);
+    
     assertEquals("HOUSTON, TX, US", driver.findElement(By.cssSelector("div.filter-text")).getText());
     assertTrue(isElementPresent(By.cssSelector("div.filter-text")));
     driver.findElement(By.cssSelector("button.btn.dropdown-toggle")).click();
     driver.findElement(By.linkText("New York, NY, US")).click();
+    
+  //Pause Test//
+    Thread.sleep(6000);
+    
     assertEquals("NEW YORK, NY, US", driver.findElement(By.cssSelector("div.filter-text")).getText());
     assertTrue(isElementPresent(By.cssSelector("div.filter-text")));
     driver.findElement(By.cssSelector("button.btn.dropdown-toggle")).click();
     driver.findElement(By.linkText("Washington, DC, US")).click();
+    
+  //Pause Test//
+    Thread.sleep(6000);
+    
     assertEquals("WASHINGTON, DC, US", driver.findElement(By.cssSelector("div.filter-text")).getText());
     assertTrue(isElementPresent(By.cssSelector("div.filter-text")));
     driver.findElement(By.id("clear-button")).click();
     assertTrue(isElementPresent(By.cssSelector("button.btn.dropdown-text")));
     assertTrue(isElementPresent(By.id("demo")));
     driver.get(baseUrl + "/gallery/concept/474/creative/294392");
+    
+  //Pause Test//
+    Thread.sleep(8000);
+    
     assertEquals("King's Hawaiian - 160x600", driver.findElement(By.cssSelector("li.active")).getText());
     driver.findElement(By.cssSelector("button.btn.dropdown-text")).click();
     assertTrue(isElementPresent(By.cssSelector("button.btn.dropdown-text")));
     driver.findElement(By.cssSelector("button.btn.dropdown-toggle")).click();
     driver.findElement(By.linkText("Afternoon")).click();
+    
+    //Pause Test//
+    Thread.sleep(8000);
+    
     assertEquals("AFTERNOON", driver.findElement(By.cssSelector("div.filter-text")).getText());
     assertTrue(isElementPresent(By.cssSelector("div.filter-text")));
     driver.findElement(By.cssSelector("button.btn.dropdown-toggle")).click();
     driver.findElement(By.linkText("Early Morning")).click();
+    
+    //Pause Test//
+    Thread.sleep(8000);
+    
     assertEquals("EARLY MORNING", driver.findElement(By.cssSelector("div.filter-text")).getText());
     assertTrue(isElementPresent(By.cssSelector("div.filter-text")));
     driver.findElement(By.cssSelector("button.btn.dropdown-toggle")).click();
     driver.findElement(By.linkText("Evening")).click();
+    
+    //Pause Test//
+    Thread.sleep(8000);
+    
     assertEquals("EVENING", driver.findElement(By.cssSelector("div.filter-text")).getText());
     assertTrue(isElementPresent(By.cssSelector("div.filter-text")));
     driver.findElement(By.cssSelector("button.btn.dropdown-toggle")).click();
     driver.findElement(By.linkText("Late Night")).click();
+    
+    //Pause Test//
+    Thread.sleep(8000);
+    
     assertEquals("LATE NIGHT", driver.findElement(By.cssSelector("div.filter-text")).getText());
     assertTrue(isElementPresent(By.cssSelector("div.filter-text")));
     driver.findElement(By.cssSelector("button.btn.dropdown-toggle")).click();
     driver.findElement(By.linkText("Morning")).click();
+    
+    //Pause Test//
+    Thread.sleep(8000);
+    
     assertEquals("MORNING", driver.findElement(By.cssSelector("div.filter-text")).getText());
     assertTrue(isElementPresent(By.cssSelector("div.filter-text")));
     driver.findElement(By.id("clear-button")).click();
+    
+    //Pause Test//
+    Thread.sleep(8000);
+    
     assertTrue(isElementPresent(By.cssSelector("button.btn.dropdown-text")));
     assertEquals("King's Hawaiian - 160x600", driver.findElement(By.cssSelector("li.active")).getText());
     driver.get(baseUrl + "/gallery");
