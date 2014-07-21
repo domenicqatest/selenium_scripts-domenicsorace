fe75ae7ab63892ac5f1d8049a51933e70bbf007c
domenicqatest <domenicqatest@gmail.com>
2014-07-21 17:16:04 -0400

    Toggle Update

--- Toggle.java
+++ Toggle.java
@@ -1,8 +1,11 @@
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
+
 import org.junit.*;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
@@ -22,20 +25,35 @@ public class Toggle {
 
   @Test
   public void testToggleJUnit4() throws Exception {
-    driver.get(baseUrl + "/gallery");
-    driver.findElement(By.id("list-view")).click();
-    assertEquals("300x250", driver.findElement(By.xpath("(//a[contains(text(),'300x250')])[8]")).getText());
-    driver.findElement(By.id("grid-view")).click();
-    driver.findElement(By.linkText("300 x 250")).click();
-    driver.findElement(By.linkText("Other")).click();
-    driver.findElement(By.xpath("(//a[contains(text(),'300 x 250')])[17]")).click();
-    driver.findElement(By.linkText("160x600")).click();
-    driver.findElement(By.linkText("728x90")).click();
-    driver.findElement(By.xpath("(//a[contains(text(),'300x250')])[2]")).click();
-    driver.findElement(By.linkText("CPG")).click();
-    driver.findElement(By.xpath("(//a[contains(text(),'Instream')])[3]")).click();
-    driver.findElement(By.linkText("Gallery")).click();
-    driver.get(baseUrl + "/gallery");
+
+      driver.get(baseUrl + "/gallery");
+      driver.findElement(By.id("list-view")).click();
+      
+      
+      //Pause//
+      Thread.sleep(1000);
+      
+      assertEquals("300x250", driver.findElement(By.xpath("(//a[contains(text(),'300x250')])[8]")).getText());
+      
+      
+      //Pause//
+      Thread.sleep(1000);
+      
+      driver.findElement(By.id("grid-view")).click();
+      
+      
+      //Pause//
+      Thread.sleep(1000);
+      
+      driver.findElement(By.linkText("300 x 250")).click();
+      driver.findElement(By.linkText("Other")).click();
+      driver.findElement(By.xpath("(//a[contains(text(),'300 x 250')])[17]")).click();
+      driver.findElement(By.linkText("160x600")).click();
+      driver.findElement(By.linkText("728x90")).click();
+      driver.findElement(By.xpath("(//a[contains(text(),'300x250')])[2]")).click();
+      driver.findElement(By.linkText("CPG")).click();
+      driver.findElement(By.xpath("(//a[contains(text(),'Instream')])[3]")).click();
+      driver.findElement(By.linkText("Gallery")).click();
   }
 
   @After
